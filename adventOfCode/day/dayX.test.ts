e35b1f686674a8dd8ae36986518689d91aec95f4
Matt Jewell <m@mattjewell.co.uk>
2022-12-01 08:42:02 +0100

    2022 day1~

--- adventOfCode/day/dayX.test.ts
+++ adventOfCode/day/dayX.test.ts
@@ -1,35 +1,22 @@
-import { readFileSync } from "node:fs"
-import { join } from "node:path"
-import { part1, part2 } from "./index"
+import { readFileSync } from "node:fs";
+import { join } from "node:path";
+import { part1, part2 } from "./index";
 
-describe("Day 01", () => {
-  const input = readFileSync(join(__dirname, "input.txt"), "utf-8")
-  const exampleInput1 = `1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000`.trim()
-  const exampleInput2 = exampleInput1
+describe.skip("Day 02", () => {
+  const input = readFileSync(join(__dirname, "input.txt"), "utf-8");
+  const exampleInput1 = ``;
+  const exampleInput2 = exampleInput1;
 
   it("should solve part 1 example", () => {
-    expect(part1(exampleInput1)).toEqual(24000)
-  })
+    expect(part1(exampleInput1)).toEqual(-1);
+  });
   it("should solve part 1", () => {
-    expect(part1(input)).toEqual(70720)
-  })
+    expect(part1(input)).toEqual(-1);
+  });
   it("should solve part 2 example", () => {
-    expect(part2(exampleInput2)).toEqual(45000)
-  })
+    expect(part2(exampleInput2)).toEqual(-1);
+  });
   it("should solve part 2", () => {
-    expect(part2(input)).toEqual(207148)
-  })
-})
+    expect(part2(input)).toEqual(-1);
+  });
+});
--- adventOfCode/day/index.ts
+++ adventOfCode/day/index.ts
@@ -1,17 +1,7 @@
-import { max, sum } from "../utils/numbers"
+export function part1(input: string) {
+  return -1;
+}
 
-export const part1 = (input: string) =>
-  max(getInput(input).map((elf) => sum(elf)))
-
-export const part2 = (input: string) =>
-  sum(
-    getInput(input)
-      .map((elf) => sum(elf))
-      .sort((a, b) => a - b)
-      .slice(-3)
-  )
-
-const getInput = (input: string): number[][] =>
-  input
-    .split("\n\n")
-    .map((elf) => elf.split("\n").map((line) => parseInt(line)))
+export function part2(input: string) {
+  return -1;
+}
