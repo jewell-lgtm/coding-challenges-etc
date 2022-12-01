cc5bd2af59997ef39b818d6be23ef4bd4ea3bb67
Matt Jewell <m@mattjewell.co.uk>
2022-12-01 08:02:34 +0100

    2022 day1

--- adventOfCode/day/index.ts
+++ adventOfCode/day/index.ts
@@ -1,7 +1,17 @@
-export function part1(input: string) {
-  return -1;
-}
+import { max, sum } from "../utils/numbers"
 
-export function part2(input: string) {
-  return -1;
-}
+export const part1 = (input: string) =>
+  max(getInput(input).map((elf) => sum(elf)))
+
+export const part2 = (input: string) =>
+  sum(
+    getInput(input)
+      .map((elf) => sum(elf))
+      .sort((a, b) => a - b)
+      .slice(-3)
+  )
+
+const getInput = (input: string): number[][] =>
+  input
+    .split("\n\n")
+    .map((elf) => elf.split("\n").map((line) => parseInt(line)))
--- adventOfCode/utils/numbers.ts
+++ adventOfCode/utils/numbers.ts
@@ -1,16 +1,26 @@
 export const sum = (numbers: number[]): number =>
-  numbers.reduce((acc, curr) => acc + curr, 0);
+  numbers.reduce((acc, curr) => acc + curr, 0)
 
 export const product = (numbers: number[]): number =>
-  numbers.reduce((acc, curr) => acc * curr, 1);
+  numbers.reduce((acc, curr) => acc * curr, 1)
 
 export const hexToBinary = (hex: string): string =>
   hex
     .split("")
     .map((char) => parseInt(char, 16).toString(2).padStart(4, "0"))
-    .join("");
+    .join("")
 
-export const binaryToDecimal = (binary: string): number => parseInt(binary, 2);
+export const binaryToDecimal = (binary: string): number => parseInt(binary, 2)
 
 export const binaryToHex = (binary: string): string =>
-  parseInt(binary, 2).toString(16);
+  parseInt(binary, 2).toString(16)
+
+export const max = (numbers: number[]): number => {
+  let max = -Infinity
+  for (const number of numbers) {
+    if (number > max) {
+      max = number
+    }
+  }
+  return max
+}
